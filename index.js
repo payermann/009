2a57078e151dfc7fc1a4a0d93c37434e13b86aeb
Felix Fischer <payermann@gmail.com>
2025-02-13 09:53:27 +0100

    add return

--- index.js
+++ index.js
@@ -13,12 +13,15 @@ const products = [
 
 const sortedProducts = products.sort((a, b) => a.price - b.price);
 console.log(sortedProducts);
-const productsWenige10 = sortedProducts.filter((product) => product.price > 10);
-const rabattProzent20 = productsWenige10.map((product) => ({
-  name: product.name,
-  price: product.price * 0.8,
-}));
+const productsWenige10 = sortedProducts.filter((product) => {
+  return product.price > 10;
+});
+const rabattProzent20 = productsWenige10.map((product) => {
+  // name: product.name,
+  product.price = product.price * 0.8;
+  return product;
+});
 const productsArray = rabattProzent20.map(
-  (product) => `${product.name}: ${product.price.toFixed(2)}€`,
+  (product) => `${product.name}: ${product.price.toFixed(2)}€`
 );
 console.log(productsArray);
